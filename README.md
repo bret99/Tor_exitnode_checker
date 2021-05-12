c41befa2a83dd490684902c8e7970f1971fe0c3f
bret99 <absolutedev@mail.ru>
2021-05-12 11:20:32 +0400

    Updated

---
+++ README.md
@@ -0,0 +1,1 @@
+It's a simple script to check whether the target IP is a tor exit node
---
+++ tor_exitnode_checker.py
@@ -0,0 +1,10 @@
+import os 
+
+os.system('wget https://check.torproject.org/torbulkexitlist?ip=1.1.1.1 -q -O tor_exit_nodes.txt')
+IP_to_check = input("Enter the exit node to check: ")
+
+with open('{}/tor_exit_nodes.txt'.format(os.getcwd())) as ten:
+    for line in ten.readlines():
+        if IP_to_check in line:
+            print('{}\033[1;91mfound!!!\033[1;00m'.format(line))
+
